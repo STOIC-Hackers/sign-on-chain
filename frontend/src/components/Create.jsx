a3029f307d7bd342870472d8c52df2c1265e46b5
Vishruth <vishruthvs2003@gmail.com>
2023-08-23 21:40:20 +0530

    design

--- frontend/src/components/Create.jsx
+++ frontend/src/components/Create.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import Header from './Header'
 import { FileUploader } from "react-drag-drop-files";
 import Footer from './Footer';
+import styles from './style.module.css'
 
 const fileTypes = ["JPG", "PNG", "GIF", "PDF"];
 
@@ -19,34 +20,50 @@ const Create = () => {
     return (
         <>
             <Header />
-            <div className='bg-gray-200 min-h-fit flex justify-center p-1'>
-                <div className='bg-white w-2/4 mt-16'>
-                    <h1 className='text-2xl mt-5 ml-5 font-semibold'>Create new esignature request</h1>
+            <div className='bg-gradient-to-bl  from-sky-900 via-gray-900 to-slate-900 min-h-fit flex justify-center p-1'>
+                <div className='bg-gray-700 w-2/4 mt-16'>
+                     
+                    <h1 className=' mt-5 ml-5 font-extrabold text-cyan-500 text-3xl'>Create new esignature request</h1>
                     <form >
                         <section>
-                            <h4 className='ml-5 mt-10 font-semibold'>Esignature request title:</h4>
-                            <input placeholder='Title of the E-signature request' className='block border-2 mt-5 h-12 ml-5 w-[96%]' type="text" name="title" id="" />
-                            <textarea placeholder='Description of the E-signature request' className='border-2 mt-2 ml-5' name="" cols="96" rows="2" />
+                            <h4 className='ml-5 mt-10 font-semibold text-cyan-200'>Esignature request title:</h4>
+                        
+                            <input
+  placeholder='Title of the E-signature request'
+  className='block border-2 mt-5 h-12 ml-5 w-[96%] max-w-full bg-black text-pink-500'
+  type="text"
+  name="title"
+  id=""
+/>
+
+<textarea
+  placeholder='Description of the E-signature request'
+  className='border-2 mt-5 ml-5 block w-[96%] max-w-full bg-black text-pink-600'
+  name=""
+  cols="96"
+  rows="2"
+/>
+
                         </section>
 
                         <section>
-                            <h4 className='ml-5 mt-10 font-semibold'>Upload Documents to E-sign:</h4>
+                            <h4 className='ml-5 mt-10 font-semibold text-whtie text-cyan-200'>Upload Documents to E-sign:</h4>
                             {/* <input type="file" name="docfile" id="" /> */}
-                            <div className='ml-5 mt-2'>
+                            <div className='ml-5 mt-2 bg-black '>
                                 <DragDrop />
                             </div>
-                            <h5 className='font-semibold ml-5 mt-2'>Files to upload:</h5>
+                            <h5 className='font-semibold ml-5 mt-2 text-cyan-200'>Files to upload:</h5>
                         </section>
 
                         <section>
-                            <h4 className='ml-5 mt-10 font-semibold'>Enter signer address:</h4>
-                            <p className='ml-5'>In order to sign or agree to the documents, the viewer or potential signer of the documents must prove ownership of a particular address</p>
-                            <input placeholder="Enter Signer's Email" className='border-2 mt-5 ml-5 w-[96%] h-12' type="email" name="signerEmail" id="" />
-                            <input placeholder='Signer Address:' className='border-2 my-5 ml-5 w-[96%] h-12' type="text" name="signerAddress" id="" />
+                            <h4 className='ml-5 mt-10 font-bold text-cyan-500 text-3xl'>Enter signer address:</h4>
+                            <p className='ml-5 text-cyan-200'>In order to sign or agree to the documents, the viewer or potential signer of the documents must prove ownership of a particular address</p>
+                            <input placeholder="Enter Signer's Email " className='border-2 mt-5 ml-5 w-[96%] h-12 text-pink-500 bg-black' type="email" name="signerEmail" id="" />
+                            <input placeholder='Signer Address:' className='border-2 my-5 ml-5 w-[96%] h-12 text-cyan-500 bg-black' type="text" name="signerAddress" id="" />
                         </section>
                     </form>
                 </div>
-                <div className='bg-white ml-8 w-80 h-64 mt-16'>
+                <div className='bg-gray-700 ml-8 w-80 h-64 mt-16 text-cyan-500'>
                     <h5>Fill in fields Enter required data.</h5>
                 </div>
             </div>
@@ -56,3 +73,4 @@ const Create = () => {
 }
 
 export default Create
+
--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -8,7 +8,7 @@ const Header = () => {
                 <Link to='/'>
                     <img className='w-44 mx-3' src={logo} alt="logo" />
                 </Link>
-                <button className='bg-blue-500 hover:bg-blue-400 mx-3 p-2 text-white '>Login with MetaMask</button>
+                <button className='text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 shadow-[0_20px_50px_rgba(8,_112,_184,_0.7)] '>Login with MetaMask</button>
                 <Link to='/create'>
                     <h5 className='mx-3 text-center hover:text-blue-400'>Create E-signature request</h5>
                 </Link>
