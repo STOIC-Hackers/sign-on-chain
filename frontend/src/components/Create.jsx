c0098ee8e0eadde767e94962e3f45f91a97c6cf0
Vishruth <vishruthvs2003@gmail.com>
2023-08-26 17:13:19 +0530

    create.jsx

--- frontend/src/components/Create.jsx
+++ frontend/src/components/Create.jsx
@@ -215,14 +215,14 @@ const Create = ({ formState }) => {
             <Header />
             <div className='bg-gradient-to-bl  from-sky-900 via-gray-900 to-slate-900 min-h-fit flex justify-center p-1'>
                 <div className='bg-gray-600 w-2/4 mt-16'>
-                    <h1 className='text-2xl mt-5 ml-5 font-semibold text-white'>Create new esignature request</h1>
+                    <h1 className='mt-5 ml-5 font-extrabold text-cyan-500 text-3xl'>Create new esignature request</h1>
                     <form onSubmit={handleForm} >
                         <section>
-                            <h4 className='ml-5 mt-10 font-semibold text-white'>Esignature request title:</h4>
+                            <h4 className='ml-5 mt-10 font-semibold text-cyan-200'>Esignature request title:</h4>
 
                             <input value={title} onChange={(e) => setTitle(e.target.value)}
                                 placeholder='Title of the E-signature request'
-                                className='block border-2 mt-5 h-12 ml-5 w-[96%] max-w-full rounded-lg'
+                                className='block border-2 mt-5 h-12 ml-5 w-[96%] max-w-full rounded-lg bg-black text-pink-500'
                                 type="text"
                                 name="title"
                                 id=""
@@ -231,7 +231,7 @@ const Create = ({ formState }) => {
 
                             <textarea value={description} onChange={(e) => setDescription(e.target.value)}
                                 placeholder='Description of the E-signature request'
-                                className='border-2 mt-5 ml-5 block w-[96%] max-w-full bg-gray-300 rounded-lg'
+                                className='border-2 mt-5 ml-5 block w-[96%] max-w-full bg-black text-pink-500 rounded-lg'
                                 name="description"
                                 cols="96"
                                 rows="2"
@@ -241,26 +241,26 @@ const Create = ({ formState }) => {
                         </section>
 
                         <section>
-                            <h4 className='ml-5 mt-10 font-semibold text-whtie text-white'>Upload Documents to E-sign:</h4>
+                            <h4 className='ml-5 mt-10 font-semibold text-cyan-200'>Upload Documents to E-sign:</h4>
                             {/* <input type="file" name="docfile" id="" /> */}
                             <div className='ml-5 mt-2'>
                                 <DragDrop useFile={useFile} />
                             </div>
-                            <h5 className='font-semibold ml-5 mt-2 text-white'>Files to upload:</h5>
+                            <h5 className='font-semibold ml-5 mt-2 text-cyan-200'>Files to upload:</h5>
                         </section>
 
                         <section>
-                            <h4 className='ml-5 mt-10 font-semibold text-white'>Enter signer address:</h4>
-                            <p className='ml-5 text-white'>In order to sign or agree to the documents, the viewer or potential signer of the documents must prove ownership of a particular address</p>
+                            <h4 className='ml-5 mt-10 font-bold text-cyan-500 text-3xl '>Enter signer address:</h4>
+                            <p className='ml-5 text-cyan-200 py-3'>In order to sign or agree to the documents, the viewer or potential signer of the documents must prove ownership of a particular address</p>
                             <input value={signerEmail} onChange={(e) => setSignerEmail(e.target.value)}
-                                placeholder="Enter Signer's Email(optional) " className='border-2 mt-5 ml-5 w-[96%] h-12 text-cyan-500' type="email" name="signerEmail" id="" />
+                                placeholder="Enter Signer's Email(optional) " className='border-2 mt-5 ml-5 w-[96%] h-12 text-pink-500 bg-black' type="email" name="signerEmail" id="" />
 
                             <input required value={signerAddress} onChange={(e) => setSignerAddress(e.target.value)}
-                                placeholder='Signer Address:' className='border-2 my-5 ml-5 w-[96%] h-12' type="text" name="signerAddress" id="" />
+                                placeholder='Signer Address:' className='border-2 my-5 ml-5 w-[96%] h-12 text-pink-500 bg-black' type="text" name="signerAddress" id="" />
 
                         </section>
                         {
-                            uploading ? <button type="submit" className='text-white bg-yellow-400  focus:ring-4 focus:outline-none  font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 shadow-[0_20px_50px_rgba(8,_112,_184,_0.7)] ml-5 '> <TailSpin color='#fff' height={20} /> Create E-signature request</button>
+                            uploading ? <button type="submit" className='text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl  focus:ring-4 focus:outline-none  font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 shadow-[0_20px_50px_rgba(8,_112,_184,_0.7)] ml-5 '> <TailSpin color='#fff' height={20} /> Create E-signature request</button>
                                 : uploadingError == true ? <button type="submit" className='text-white bg-red-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 shadow-[0_20px_50px_rgba(8,_112,_184,_0.7)] ml-5 '>error</button> :
                                     uploaded == false ?
                                         <button type="submit" className='text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 shadow-[0_20px_50px_rgba(8,_112,_184,_0.7)] ml-5 '>Create E-signature request</button>
@@ -269,15 +269,15 @@ const Create = ({ formState }) => {
                         {
                             uploaded ? <div>
                                 <h4 className='text-green-400 ml-5'>Created esignature request!</h4>
-                                <h4 className='text-blue-500 ml-5'><a href={`https://ipfs.io/ipfs/${imgHash}`}>View metadata</a></h4>
-                                <h4 className='text-blue-500 ml-5'><a href={`https://mumbai.polygonscan.com/address/${contractAddress}`}>View created contract</a></h4>
-                                <h4 className='mt-5 ml-5'>Share this url with the potential signer:</h4>
-                                <Link className='ml-5 text-blue-500' to={`/sign/${imgHash}/${contractAddress}`}> Open eSignature url</Link>
+                                <h4 className='text-cyan-500 ml-5'><a href="">View metadata</a></h4>
+                                <h4 className='text-cyan-500 ml-5'><a href="">View created contract</a></h4>
+                                <h4 className='mt-5 ml-5 text-cyan-500'>Share this url with the potential signer:</h4>
+                                <Link className='ml-5 text-blue-500' to={`/sign/${imgHash}`}> Open eSignature url</Link>
                             </div> : null
                         }
                     </form>
                 </div>
-                <div className='bg-white ml-8 w-80 h-64 mt-16 text-white'>
+                <div className='bg-gray-600 ml-8 w-80 h-64 mt-16 text-cyan-500 font-semibold'>
                     <h5>Fill in fields Enter required data.</h5>
                 </div>
             </div>
@@ -288,3 +288,4 @@ const Create = ({ formState }) => {
 
 export default Create
 
+//text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 shadow-[0_20px_50px_rgba(8,_112,_184,_0.7)]
